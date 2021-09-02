9b421a602d70065756219c3392757dbe20946753
Sameer Joshi <37546214+default-sameer@users.noreply.github.com>
2021-09-02 16:02:02 +0545

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     UPLOAD_FOLDER = UPLOAD_FOLDER
     MAX_CONTENT_LENGTH = 1024 * 1024
     UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif']
-    SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
     #     'sqlite:///' + os.path.join(basedir, 'image.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
