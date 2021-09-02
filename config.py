2382dbe87e4f2f8f6b42d4b51505952c876b4800
Sameer Joshi <37546214+default-sameer@users.noreply.github.com>
2021-09-02 15:59:14 +0545

    Database Config

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     UPLOAD_FOLDER = UPLOAD_FOLDER
     MAX_CONTENT_LENGTH = 1024 * 1024
     UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif']
-    SQLALCHEMY_DATABASE_URI = 'postgresql://pwwgkcaaevafbv:5ead1012ac5933ad93a32e122e7d1a7b538a225095a592cfe87c6e0af4213c1d@ec2-44-197-40-76.compute-1.amazonaws.com:5432/d1cnfs6q3001su'
+    SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
     #     'sqlite:///' + os.path.join(basedir, 'image.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
